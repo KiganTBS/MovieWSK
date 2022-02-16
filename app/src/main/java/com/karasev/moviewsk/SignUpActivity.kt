735728381ae74fba53dcdae0a84648cce4ad4388
kigan <kigan222@mail.ru>
2022-02-16 17:36:44 +0300

    Add new fun

--- app/src/main/java/com/karasev/moviewsk/SignUpActivity.kt
+++ app/src/main/java/com/karasev/moviewsk/SignUpActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
@@ -22,26 +23,84 @@ class SignUpActivity : AppCompatActivity() {
 
         val email = findViewById<EditText>(R.id.editTextEmail)
         val pass = findViewById<EditText>(R.id.editTextPassword)
+        val tvResetPassword = findViewById<TextView>(R.id.tvResetPass)
         val signUpButton = findViewById<Button>(R.id.buttonSignUp)
+        val loginButton = findViewById<Button>(R.id.buttonLogin)
 
+        signInButton(loginButton, email, pass)
+        signUpButton(signUpButton, email, pass)
+        resetPasswordFunc(tvResetPassword, email)
+
+
+    }
+
+    private fun resetPasswordFunc(
+        tvResetPassword: TextView,
+        email: EditText
+    ) {
+        tvResetPassword.setOnClickListener {
+            auth.setLanguageCode("ru")
+            auth.sendPasswordResetEmail(email.text.toString().trim())
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Log.d("RESETPASSWORD", "Email sent.")
+                    }else{
+                        Log.d("RESETPASSWORD", task.exception.toString())
+                    }
+                }
+        }
+    }
+
+    private fun signInButton(
+        loginButton: Button,
+        email: EditText,
+        pass: EditText
+    ) {
+        loginButton.setOnClickListener {
+            auth.signInWithEmailAndPassword(
+                email.text.toString().trim(),
+                pass.text.toString().trim()
+            )
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("SIGNIN", "signInWithEmail:success")
+                        val user = auth.currentUser
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("SIGNIN", "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+    }
+
+    private fun signUpButton(
+        signUpButton: Button,
+        email: EditText,
+        pass: EditText
+    ) {
         signUpButton.setOnClickListener {
 
-            auth.createUserWithEmailAndPassword(email.text.toString(), pass.text.toString())
+            auth.createUserWithEmailAndPassword(
+                email.text.toString().trim(),
+                pass.text.toString().trim()
+            )
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
                         Log.d("MovieDB", "Success")
                         val user = auth.currentUser
-
                     } else {
                         Toast.makeText(
-                            baseContext, "Authentication failed.$pass $email",
+                            baseContext, "${task.exception} \n$pass $email",
                             Toast.LENGTH_SHORT
                         ).show()
 
                     }
                 }
         }
-
-
     }
 }
